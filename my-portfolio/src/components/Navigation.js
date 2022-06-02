2417bf570d45cd09a5a864f0150e5064655aca86
Nicholas Amideo <namideo@Nicholass-MacBook-Pro.local>
2022-06-01 22:03:00 -0400

    readme

--- my-portfolio/src/components/Navigation.js
+++ my-portfolio/src/components/Navigation.js
@@ -2,12 +2,12 @@ import React from 'react';
 
     function Navigation({ currentPage, handlePageChange }) {
         return (
-          <ul className="links">
-            <li><a href="#About-Me" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a></li>
-            <li><a href="#Work"onClick={() => handlePageChange('Work')}className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}>Work</a></li>
-            <li><a href="https://docs.google.com/document/d/1PNZUu_1SKusdyYGTgADt8nkzXUwfCI5wlc4iJBGvH6c/edit?usp=sharing" target="_blank">Resume</a></li>
-            <li><a href="#contactInfo"onClick={() => handlePageChange('Contact')}className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a></li>
-          </ul>
+          <div className="links">
+            <a href="#About-Me" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a>
+            <a href="#Work" onClick={() => handlePageChange('Work')}className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}>Work</a>
+            <a href="https://docs.google.com/document/d/1PNZUu_1SKusdyYGTgADt8nkzXUwfCI5wlc4iJBGvH6c/edit?usp=sharing" target="_blank" className='nav-link'>Resume</a>
+            <a href="#contactInfo" onClick={() => handlePageChange('Contact')}className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
+          </div>
         );
       }
 
