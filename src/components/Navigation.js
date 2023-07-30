f62548037f060d58ba128de33753239a294f1fdb
namideo <namideo@gmail.com>
2023-07-30 16:30:45 -0400

    updated resume

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import React from 'react';
           <div className="links">
             <a href="#About-Me" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a>
             <a href="#Work" onClick={() => handlePageChange('Work')}className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}>Work</a>
-            <a href="https://docs.google.com/document/d/1PNZUu_1SKusdyYGTgADt8nkzXUwfCI5wlc4iJBGvH6c/edit?usp=sharing" target="_blank" className='nav-link'>Resume</a>
+            <a href="https://docs.google.com/document/d/1k_m0bp0y-BZu5olOfYu_mUF33nP4D6tg/edit" target="_blank" className='nav-link'>Resume</a>
             <a href="#contactInfo" onClick={() => handlePageChange('Contact')}className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
           </div>
         );
